57aaa8c43de90de604415892182b8b70a48ba84e
ckaraboran <karabaron@gmail.com>
2022-12-10 19:57:10 +0100

    fix apply migration shell file

--- Scripts/Add-Migrations.ps1
+++ Scripts/Add-Migrations.ps1
@@ -1,6 +1,6 @@
 [string]$MigrationName = $null
 while ([string]::IsNullOrWhitespace($MigrationName)) {
-	$MigrationName = Read-Host "Migration name"
+	$MigrationName = Read-Host "Migration name" 
 }
 
 dotnet ef migrations add $MigrationName --project ../Libraries/KeyManager.Infrastructure -s ../Presentation/KeyManager.Api/KeyManager.Api.csproj
