8fff42fd910391da2df27109466e38a447699718
ckaraboran <karabaron@gmail.com>
2022-12-11 14:46:44 +0100

    add user role table

--- Libraries/KeyManager.Infrastructure/DataContext.cs
+++ Libraries/KeyManager.Infrastructure/DataContext.cs
@@ -15,6 +15,7 @@ public class DataContext : DbContext
     public DbSet<Permission> Permissions { get; set; }
     public DbSet<Role> Roles { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<UserRole> UsersRoles { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -24,6 +25,7 @@ public class DataContext : DbContext
         modelBuilder.ApplyConfiguration(new EntityMapBase<Permission>());
         modelBuilder.ApplyConfiguration(new EntityMapBase<Role>());
         modelBuilder.ApplyConfiguration(new EntityMapBase<User>());
+        modelBuilder.ApplyConfiguration(new EntityMapBase<UserRole>());
         base.OnModelCreating(modelBuilder);
     }
 
